2357ee7dd098dcaa23f128d0cc8ef4ccc15a8d29
Victor Cardoso <victorclmatos@gmail.com>
2024-07-17 23:20:01 -0300

    ajustes da responsividade parte 1

--- src/componentes/Grupos/Grupo/index.js
+++ src/componentes/Grupos/Grupo/index.js
@@ -1,22 +1,25 @@
 import Titulo from 'componentes/Titulo';
 import Cavaleiro from './Cavaleiros';
 import styles from './Grupo.module.css';
+import { Scrollbar } from 'react-scrollbars-custom';
 
 function Grupo({ titulo, backgroundColor, borderColor, borderColorBottom, boxShadow }) {
     
     return (
-        <div className={styles.grupoContainer}>
+        <div className={styles.grupoContainer} >
+            <div className={styles.titulo}>
                 <Titulo 
                     backgroundColor={backgroundColor} 
                     titulo={titulo}
-                />
+                    />
+            </div>
             <div className={styles.grupo}>
-                <Cavaleiro 
-                    borderColor={borderColor} 
-                    borderColorBottom={borderColorBottom}
-                    boxShadow={boxShadow}
-                    titulo={titulo} 
-                />
+                    <Cavaleiro 
+                        borderColor={borderColor} 
+                        borderColorBottom={borderColorBottom}
+                        boxShadow={boxShadow}
+                        titulo={titulo} 
+                    />
             </div>
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.module.css';
+import './index.css';
 import AppRoutes from './routes.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
