8002832c49895b39e82a79dc857ad54ebb036628
Victor Cardoso <victorclmatos@gmail.com>
2024-07-18 14:42:23 -0300

    ajustes pagina nao encontrada

--- src/pages/PaginaNaoEncontrada/index.js
+++ src/pages/PaginaNaoEncontrada/index.js
@@ -12,11 +12,11 @@ function PaginaNaoEncontrada() {
             </h1>
 
             <p className={styles.paragrafo}>
-                Tem certeza de que era isso que você estava procurando? Verifique o endereço fornecido na barra de endereço ou no endereço da imagem.
+                Tem certeza de que era isso que você estava procurando? Verifique o endereço fornecido na barra de endereços ou no endereço da imagem do card.
             </p>
 
             <p className={styles.paragrafo}>
-                Aguarde uns instantes e recarregue a página, volte para a página inicial ou coloque um link válido.
+                Aguarde uns instantes e recarregue a página, volte para a página inicial ou coloque um link válido na barra de endereços ou no endereço da imagem do card.
             </p>
         </div>
     )
